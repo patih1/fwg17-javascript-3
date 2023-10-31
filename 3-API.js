e6be925e79bc6961f95002682e933252ea2fbc3b
haidar <143616075+patih1@users.noreply.github.com>
2023-11-01 04:41:03 +0700

    Add files via upload

--- 3-API.js
+++ 3-API.js
@@ -4,8 +4,14 @@ async function ambilNama() {
   try{
     const res = await data
     const nama = await res.json()
-    const {name} = nama
-    return console.log(name)
+    // const [name] = nama
+    // return console.log(nama)
+    let result = ''
+    for(i = 0; i < nama.length; i++){
+      result += nama[i].name
+    }
+    console.log(result)
+
   }catch(error){
     throw error
   }
