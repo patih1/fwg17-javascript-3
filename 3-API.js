dcb3fce2b80c12e7350883b34845c5c1e96110be
haidar <143616075+patih1@users.noreply.github.com>
2023-11-01 16:57:24 +0700

    Add files via upload

--- 3-API.js
+++ 3-API.js
@@ -4,8 +4,6 @@ async function ambilNama() {
   try{
     const res = await data
     const nama = await res.json()
-    // const [name] = nama
-    // return console.log(nama)
     let x = ''
     let result = []
     if(nama.length){
@@ -24,4 +22,41 @@ async function ambilNama() {
 }
 
 ambilNama()
+
+//--------------------------------------------------------------------------------------------------------------------------------//
+
+const ifError = (param) => {
+  if(param.message === 'Invalid response'){
+    console.log('request is invalid')
+  } else {
+    console.group(param.message)
+  }
+}
+
+const getData = (url, cb) => {
+  fetch(url).then(res => {
+      res.json().then(
+        res2 => {
+          if(res2.length){
+            cb(res2)
+          }else{
+            throw new Error('Invalid response')
+          }
+        }
+      ).catch(ifError)
+  }).catch(ifError)
+}
+
+function getName(param) {
+    let x = ''
+    let result = []
+  for(i = 0; i < param.length; i++){
+    x += param[i].name
+    result = [...result, x]
+    x = ''
+  }
+  console.log(result)
+}
+
+getData('https://jsonplaceholder.typicode.com/users', getName)
 
